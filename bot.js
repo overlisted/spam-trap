5c5fc04b413ccf3197d129959c4abf71e5507c0b
Ilya Maximov <mail@overlisted.net>
2022-01-25 13:24:44 +0100

    add logging

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 
-const { Client, Intents, Permissions } = require("discord.js");
+const { Client, Intents, Permissions, MessageEmbed} = require("discord.js");
 const { open } = require("fs/promises");
 
 const main = async () => {
   console.info("Reading the configs");
   const configFd = await open("config.json", "r+");
+  const logConfigFd = await open("logConfig.json", "r+");
+
   const config = JSON.parse(await configFd.readFile());
+  const logConfig = JSON.parse(await logConfigFd.readFile());
 
   const discord = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -18,6 +21,7 @@ const main = async () => {
     if(!msg.inGuild()) return;
 
     const entry = config[msg.guildId];
+    const logEntry = logConfig[msg.guildId];
 
     if(entry) {
       if(entry.channel === msg.channelId) {
@@ -29,6 +33,23 @@ const main = async () => {
         } catch {}
 
           await msg.member.ban({ days: 1, reason: "Sent a message in the trap channel" });
+
+        if(logEntry && logEntry.channel) {
+          try {
+            const logs = await discord.channels.fetch(logEntry.channel);
+            await logs.send({
+              embeds: [
+                new MessageEmbed()
+                  .setTitle("Someone was banned!")
+                  .addField("Tag", msg.member.user.tag, true)
+                  .addField("ID", msg.member.user.id, true)
+                  .addField("Message", msg.content, false)
+              ]
+            });
+          } catch(e) {
+            console.error(e);
+          }
+        }
       }
     }
   });
@@ -51,6 +72,24 @@ const main = async () => {
       const newData = JSON.stringify(config);
       await configFd.write(newData, 0);
       await configFd.truncate(newData.length);
+
+      await interaction.reply("Saved!");
+    }
+
+    if(interaction.commandName === "logs") {
+      if(!interaction.memberPermissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+        await interaction.reply(`You need the "Ban members" permission to use this.`);
+
+        return;
+      }
+
+      logConfig[interaction.guildId] = {
+        channel: interaction.options.getChannel("channel").id,
+      };
+
+      const newData = JSON.stringify(logConfig);
+      await logConfigFd.write(newData, 0);
+      await logConfigFd.truncate(newData.length);
 
       await interaction.reply("Saved!");
     }
--- setup.js
+++ setup.js
@@ -4,24 +4,38 @@ const { REST } = require("@discordjs/rest");
 const { Routes, ApplicationCommandOptionType } = require("discord-api-types/v9");
 const { writeFile } = require("fs/promises");
 
-const commands = [{
-  name: "trap",
-  description: "Configures the bot",
-  options: [
-    {
-      name: "channel",
-      description: "The trap channel in this server",
-      type: ApplicationCommandOptionType.Channel,
-      required: true
-    },
-    {
-      name: "ban_msg",
-      description: "The message that will be sent to banned people",
-      type: ApplicationCommandOptionType.String,
-      required: true
-    }
-  ]
-}];
+const commands = [
+  {
+    name: "trap",
+    description: "Configures the bot",
+    options: [
+      {
+        name: "channel",
+        description: "The trap channel in this server",
+        type: ApplicationCommandOptionType.Channel,
+        required: true
+      },
+      {
+        name: "ban_msg",
+        description: "The message that will be sent to banned people",
+        type: ApplicationCommandOptionType.String,
+        required: true
+      }
+    ]
+  },
+  {
+    name: "logs",
+    description: "Configures the log channel",
+    options: [
+      {
+        name: "channel",
+        description: "The log channel in this server",
+        type: ApplicationCommandOptionType.Channel,
+        required: true
+      }
+    ]
+  }
+];
 
 const main = async () => {
   const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
@@ -39,6 +53,7 @@ const main = async () => {
 
   console.log("=> Config file");
   await writeFile("config.json", "{}");
+  await writeFile("logConfig.json", "{}");
 };
 
 main()
